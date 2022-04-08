1207ebbe372fe6feb2d3fb0757d5031f77e2f6a5
Kaitlyn Greve <kgreve14@gmail.com>
2022-04-07 20:50:23 -0400

    commit tree 9e89e5ff48980b2c9a925ebb79ca6cb9212cda3f

--- index.js
+++ index.js
@@ -11,6 +11,43 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   const newArr = tutorials.map(tutorial => {
+//    return console.log(tutorial.split(' ')); // each word is now its own string  
+//   });
+//   console.log(newArr)
+//   return newArr.map(nestedArr => {
+//     return nestedArr.map(word => {
+//       return (word[0].toUpperCase() + word.substring(1))
+//     }).join(' ');
+    
+//   });
+  
+// };
+
+// console.log(titleCased());
+
+
+
+// function format(string) {
+//   const eachWord = string.split(' ');
+//   const newArrCaps = eachWord.map(word => {
+//     return word[0].toUpperCase() + word.substring(1)
+//   })
+//   return newArrCaps.join(' ');
+// }
+
+function titleCasedProcessor(inputArray) {
+  return inputArray.map(item => function(){
+    item.toLowerCase();
+    let splitItem = item.split(' ');
+    splitItem.map(item => (
+      item.substring(1).toUpperCase()
+    ));
+  });
 }
+
+function titleCased() {
+  return titleCasedProcessor(tutorials);
+}
+
